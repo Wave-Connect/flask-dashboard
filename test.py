b8b243e416519aff682f070b42bd80748c09ad6b
Matt <mattbingham@users.noreply.github.com>
2020-09-02 12:56:23 +0200

    Amended tests.py

--- test.py
+++ test.py
@@ -1,22 +1,36 @@
+#!/usr/bin/env python
 from datetime import datetime, timedelta
 import unittest
-from app import app, db
+from app import create_app, db
 from app.models import User
+from config import Config
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
         db.create_all()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+        self.app_context.pop()
 
     def test_password_hashing(self):
-        u = User(username='susan')
-        u.set_password('cat')
-        self.assertFalse(u.check_password('dog'))
-        self.assertTrue(u.check_password('cat'))
+        u = User(
+            username='matt',
+            fname='Matthew',
+            lname='Bingham')
+        u.set_password('chicken')
+        self.assertFalse(u.check_password('rooster'))
+        self.assertTrue(u.check_password('chicken'))
 
     def test_avatar(self):
         u = User(username='john', email='john@example.com')
